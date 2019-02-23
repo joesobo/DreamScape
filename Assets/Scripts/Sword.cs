686691ac74813133df74e7b07829d934b34b94c2
joesobo <soboleskijoe@gmail.com>
2019-02-22 23:41:51 -0800

    added swinging sword

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    private bool isRot = false;
+    private Vector3 startRot;
+
+    private int speed = 1000;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            startRot = this.transform.localEulerAngles;
+            isRot = true;
+        }
+
+        if (isRot)
+        {
+            rotateSword();
+        }
+
+        if (this.transform.localEulerAngles.z < startRot.z && this.transform.localEulerAngles.z > 150)
+        {
+            this.transform.localEulerAngles = startRot;
+            isRot = false;
+        }
+    }
+
+    private void rotateSword()
+    {
+        this.transform.Rotate(new Vector3(0, 0, speed * Time.deltaTime));
+    }
+}
