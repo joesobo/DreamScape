758889211cc6a50740e8cc40adc98257ea767abf
joesobo <soboleskijoe@gmail.com>
2019-03-04 17:52:34 -0800

    added player damage effect

--- Assets/Scripts/Enemy/EnemyDamagerOnCollision.cs
+++ Assets/Scripts/Enemy/EnemyDamagerOnCollision.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class EnemyDamagerOnCollision : MonoBehaviour {
 
+    public GameObject playerBlood;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             collision.gameObject.GetComponent<HealthController>().health--;
+            Instantiate(playerBlood, collision.transform.position, Quaternion.identity);
         }
     }
 }
